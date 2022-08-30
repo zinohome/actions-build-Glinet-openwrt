336b0f976f48ce0a1c75c86b1574ec9218e9a338
ZhangJun <13583285152@139.com>
2022-08-30 18:13:24 +0800

    Update public.sh

--- openwrt/public/public.sh
+++ openwrt/public/public.sh
@@ -19,6 +19,9 @@ filter_url='https://github.com/destan19/OpenAppFilter.git'                  # Ap
 # echo "修改机器名称"
 # sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
 
+cp Makefile tools/xxd/Makefile
+cp elfMakefile tools/elfutils/Makefile
+
 # echo "修改wifi名称"
 # sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
